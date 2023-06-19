d8006f9216aac37decc27a0e995c098312085c91
shelif26 <shelifshelif11@gmail.com>
2023-06-19 15:38:21 +0530

    intial setup

---
+++ src/main/java/com/graphql/learn/entities/Provider.java
@@ -0,0 +1,50 @@
+package com.graphql.learn.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "provider")
+public class Provider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+
+    public Provider() {
+    }
+
+    public Provider(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
